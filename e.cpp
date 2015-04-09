9abd0fc294d1aba0aa8842b5c8ababefcf0b2ed1
mauriciocoopera <A01630042@itesm.mx>
2015-04-08 22:14:26 -0500

    WSQ14
    Estimating the value of e

---
+++ e.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double calculate_e(int n)
+{
+  
+  double res = 2.0;
+  double fact = 1;
+
+  for (int i=2; i<n; i++)
+  {
+    fact *= i;
+    res += 1/fact;
+  }
+
+  return res;
+}
+
+int main()
+{
+  int precision;
+  cout << endl;
+  cout << "Estimating the constant e " << endl;
+  cout << "Enter the number of significant figures you want " << endl;
+  cin >> precision;
+  cout << "The value of e is: " << setprecision(precision) << calculate_e(50) << endl;
+
+  return 0;
+}
